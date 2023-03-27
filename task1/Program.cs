226c1fd81479da42681dc1d286e52ee07f14da89
Yakovleva Larisa <lara.yakovleva.98.98@mail.ru>
2023-03-27 18:21:11 +0700

    second task

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] inputArray = new string[] { "hello", "world", "this", "is", "a", "test" }; 
+string[] outputArray = new string[inputArray.Length];  
+int j = 0;  
+ 
+for (int i = 0; i < inputArray.Length; i++)  
+{ 
+    if (inputArray[i].Length <= 3)  
+    { 
+        outputArray[j] = inputArray[i];  
+        j++;  
+    } 
+} 
+ 
+for (int i = 0; i < j; i++)  
+{ 
+    Console.WriteLine(outputArray[i]);  
+}
